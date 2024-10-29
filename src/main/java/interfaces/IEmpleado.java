dd85e713547567c14e4b5994a9be90e1c5c4d781
Erick Rivera <yomellamoerick.771@gmail.com>
2024-10-29 20:06:38 -0300

    excepciones apliadas 1

--- src/main/java/interfaces/IEmpleado.java
+++ src/main/java/interfaces/IEmpleado.java
@@ -1,14 +1,17 @@
 package interfaces;
 import java.util.List;
+
+import excepciones.EmpleadoNoEncontradoException;
+import excepciones.EmpleadoYaExisteException;
 import modelo.Empleado;
 
 public interface IEmpleado {
 
   // El equivalente a resources :empleado
-  public void crearEmpleado(Empleado emp);
-  public Empleado obtenerEmpleado(String rut);
+  public void crearEmpleado(Empleado emp) throws EmpleadoYaExisteException;
+  public Empleado obtenerEmpleado(String rut) throws EmpleadoNoEncontradoException;
   public List<Empleado> listarEmpleados();
   public void actualizarEmplado(Empleado empleado);
-  public void eliminarEmpleado(String rut);
+  public void eliminarEmpleado(String rut) throws EmpleadoNoEncontradoException;
 
 }
--- src/main/java/servicios/EmpleadoService.java
+++ src/main/java/servicios/EmpleadoService.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import excepciones.EmpleadoNoEncontradoException;
+import excepciones.EmpleadoYaExisteException;
 import interfaces.IEmpleado;
 import modelo.Empleado;
 
@@ -13,16 +15,31 @@ public class EmpleadoService implements IEmpleado {
   private Map<String, Empleado> empleados = new HashMap<>(); // empleados = {"11.222.333-4": Empleado@3544obj}
 
   @Override
-  public void crearEmpleado(Empleado emp) {
+  public void crearEmpleado(Empleado emp) throws EmpleadoYaExisteException {
+
+    // Si el conjunto de datos contiene este valor
+    if(empleados.containsKey(emp.getRut())){
+      throw new EmpleadoYaExisteException("El rut del nuevo empleado ya esta registrado.");
+    }
+
     // El map no acepta datos repetidos, solo una persona por rut
     empleados.put(emp.getRut(), emp);
+    System.out.println();
     System.out.println("Empleado creado con exito!!!!");
+    System.out.println();
   }
 
   @Override
-  public Empleado obtenerEmpleado(String rut) {
+  public Empleado obtenerEmpleado(String rut) throws EmpleadoNoEncontradoException {
+
+    Empleado emp = empleados.get(rut);
+
+    if(emp == null){
+      throw new EmpleadoNoEncontradoException("RUT de empleado no encontrado...");
+    }
+
     // Obtener por el rut particular
-    return empleados.get(rut);
+    return emp;
   }
 
   @Override
@@ -42,13 +59,13 @@ public class EmpleadoService implements IEmpleado {
   }
 
   @Override
-  public void eliminarEmpleado(String rut) {
+  public void eliminarEmpleado(String rut) throws EmpleadoNoEncontradoException {
     // Podemos usar el metodo anterior
     if (empleados.containsKey(rut)){
       empleados.remove(rut);
       System.out.println("Empleado eliminado con exito");
     } else {
-      System.out.println("EMPLEADO NO ENCONTRADO.");
+      throw new EmpleadoNoEncontradoException("EMPLEADO NO ENCONTRADO...");
     }
   }
 
